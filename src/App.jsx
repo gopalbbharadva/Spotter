11c64cb662163bba9c101eb5da824130779ce5d1
gopal <gopalbbharadva@gmail.com>
2022-04-09 13:43:37 +0530

    setup mockbee and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App flex justify-center align-middle">
+      <button className="bg-blue-500 hover:bg-blue-600 text-white text-lg p-3 rounded m-4 ">
+        Tailwind testing
+      </button>
+    </div>
+  );
+};
+
+export default App;
