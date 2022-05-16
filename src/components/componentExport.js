75e087f942a177ef0a4a4591940709aafff7dfc0
gopal <gopalbbharadva@gmail.com>
2022-05-16 16:09:43 +0530

    Export: Component export updated

--- src/components/componentExport.js
+++ src/components/componentExport.js
@@ -1,7 +1,19 @@
+import { Sidebar } from "../page/Posts/components/Sidebar/Sidebar";
+import { ProfileModal } from "../page/Profile/components/ProfileModal";
+import { Loader } from "./Loader/Loader";
 import { Navbar } from "./Navbar/Navbar";
 import { NotifyModal } from "./NotifyModal/NotifyModal";
 import { PostCard } from "./Postcard/PostCard";
 import { PostModal } from "./Postmodal/PostModal";
 import { User } from "./User/User";
 
-export { PostCard, Navbar, User, PostModal, NotifyModal };
+export {
+  PostCard,
+  Navbar,
+  User,
+  PostModal,
+  NotifyModal,
+  Loader,
+  ProfileModal,
+  Sidebar,
+};
