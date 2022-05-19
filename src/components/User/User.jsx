e969cb62cf83b925e841ba87fbe9d1b5aec2cd10
gopal <gopalbbharadva@gmail.com>
2022-05-19 18:04:54 +0530

    Refactor: Refactor User component.

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { followUser, unFollowUser } from "../../features/usersSlice";
-import { DummyAvatar } from "../componentExport";
+import { Avatar } from "../Avatar/Avatar";
 
 export const User = ({ suggestedUser }) => {
-  const { _id, firstname, lastname, profileImg, username } = suggestedUser;
+  const { _id, username } = suggestedUser;
   const dispatch = useDispatch();
   const {
     auth: { user, token },
@@ -14,31 +14,13 @@ export const User = ({ suggestedUser }) => {
   const currentUser = users?.find(({ username }) => username === user.username);
 
   const isFollowing = currentUser?.following.find(
-    (user) => suggestedUser.username === user.username
+    (user) => suggestedUser?.username === user.username
   );
 
   return (
     <div className="mt-5 flex justify-between w-full items-center cursor-pointer">
       <Link to={`/profile/${username}`}>
-        <div className="flex">
-          <div className="w-10 h-10">
-            {!profileImg ? (
-              <DummyAvatar firstname={firstname} lastname={lastname} />
-            ) : (
-              <img
-                className="profile-avatar"
-                src={profileImg}
-                alt="user profile"
-              />
-            )}
-          </div>
-          <div className="ml-3 text-sm flex flex-col items-start">
-            <p>{username}</p>
-            <p className=" text-gray-500">
-              {firstname} {lastname}
-            </p>
-          </div>
-        </div>
+        <Avatar currentUserName={username} />
       </Link>
       {isFollowing ? (
         <button
